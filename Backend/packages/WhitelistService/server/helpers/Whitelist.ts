ae355067cf8b8d736dfdd4e8fb863a2aa6188e54
Cory Cherven <corycherven@hotmail.com>
2022-02-23 01:31:29 -0500

    Moves whitelist to JSON

--- Backend/packages/WhitelistService/server/helpers/Whitelist.ts
+++ Backend/packages/WhitelistService/server/helpers/Whitelist.ts
@@ -1,23 +1,8 @@
-import fs from 'fs';
 import Web3 from 'web3';
-
-const getWhitelistFile = (): string => {
-    const wlDir = process.env.WHITELIST_DIR;
-    if (!wlDir) {
-        console.error('Whitelist not found');
-        process.exit(1);
-    }
-
-    return fs.readFileSync(wlDir).toString();
-};
+import Whitelist from '../data/GAC_Whitelist.json';
 
 export const getWhitelist = (): string[] => {
-    const wlFile = getWhitelistFile();
-
-    return wlFile
-        .split('\n')
-        .map((v) => v.trim())
-        .filter(Web3.utils.isAddress);
+    return Whitelist.map((v) => v.trim()).filter(Web3.utils.isAddress);
 };
 
 export default getWhitelist;
