52fa998bcd3d445d9e02ce1985af2885771e2818
Cory Cherven <corycherven@hotmail.com>
2022-03-13 13:51:16 -0400

    Updates background

--- Frontend/packages/market/src/organisms/MarketplacePage.tsx
+++ Frontend/packages/market/src/organisms/MarketplacePage.tsx
@@ -1,4 +1,5 @@
 import {
+    CoverVideo,
     Header,
     HeaderButtonProps,
     MOBILE,
@@ -17,6 +18,7 @@ import { useGamingApeContext } from '../contexts/GamingApeClubContext';
 import { ListingDetails } from '../molecules/ListingDetails';
 import { ListingGrid } from '../molecules/ListingGrid';
 import { ListingModal } from '../molecules/ListingModal';
+import BackgroundVideo from '../assets/webm/ComputerLights.webm';
 
 export const MarketplacePage = (): JSX.Element => {
     const [css] = useStyletron();
@@ -90,102 +92,119 @@ export const MarketplacePage = (): JSX.Element => {
     return (
         <div
             className={css({
-                display: 'flex',
-                flexDirection: 'column',
                 overflow: 'hidden',
+                position: 'relative',
                 height: '100%',
-                backgroundColor: theme.backgroundColor.dark.toRgbaString(),
-                [MOBILE]: {
-                    overflow: 'auto',
-                },
+                width: '100%',
             })}
         >
-            {selectedListing && (
-                <ListingModal
-                    listing={selectedListing}
-                    onClose={(): void => setSelectedIndex(undefined)}
-                />
-            )}
-            <Header
-                additionalButtons={additionalButtons}
-                homeUrl={homeUrl}
-                discordUrl={discordUrl}
-                openseaUrl={openseaUrl}
-                twitterUrl={twitterUrl}
-                className={css({
-                    [MOBILE]: {
-                        zIndex: 999999,
-                    },
-                    position: 'relative !important' as never,
-                    marginBottom: '20px !important',
-                })}
+            <CoverVideo
+                url={BackgroundVideo}
+                aspectRatio={1.777777}
+                className={css({ zIndex: -1 })}
+                autoplay
+                loop
             />
-            {!adminPanelOpen && (
-                <>
-                    <div
-                        className={css({
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            marginBottom: '20px',
-                        })}
-                    >
-                        <Web3ConnectButton
-                            connectModalOpen={modalOpen}
-                            setConnectModalOpen={setModalOpen}
-                            className={css({
-                                marginLeft: 'auto',
-                                [MOBILE]: { marginRight: '5px' },
-                            })}
-                        />
-                        <BalanceWidget
-                            className={css({
-                                margin: '0px 10px',
-                                [MOBILE]: { display: 'none' },
-                            })}
-                        />
-                        <DiscordLoginButton
-                            className={css({
-                                marginRight: 'auto',
-                                [MOBILE]: { marginLeft: '5px' },
-                            })}
-                        />
-                    </div>
-                    <div
-                        className={css({
-                            display: 'none',
-                            [MOBILE]: { display: 'block' },
-                        })}
-                    >
-                        <BalanceWidget
-                            className={css({
-                                margin: '10px',
-                            })}
-                        />
-                    </div>
-                    <ListingGrid
-                        className={css({
-                            flex: 1,
-                            overflow: 'auto',
-                            [MOBILE]: { overflow: 'unset' },
-                        })}
-                        onSelect={onItemSelect}
-                        request={listingRequest}
-                    />
-                </>
-            )}
-            {adminPanelOpen && (
-                <div
-                    className={css({
+            <div
+                className={css({
+                    display: 'flex',
+                    flexDirection: 'column',
+                    overflow: 'hidden',
+                    height: '100%',
+                    backgroundColor:
+                        theme.backgroundColor.dark.toRgbaString(0.8),
+                    [MOBILE]: {
                         overflow: 'auto',
-                        flex: '1',
-                        margin: '40px',
-                        [MOBILE]: { margin: '10px' },
+                    },
+                })}
+            >
+                {selectedListing && (
+                    <ListingModal
+                        listing={selectedListing}
+                        onClose={(): void => setSelectedIndex(undefined)}
+                    />
+                )}
+                <Header
+                    additionalButtons={additionalButtons}
+                    homeUrl={homeUrl}
+                    discordUrl={discordUrl}
+                    openseaUrl={openseaUrl}
+                    twitterUrl={twitterUrl}
+                    className={css({
+                        [MOBILE]: {
+                            zIndex: 999999,
+                        },
+                        position: 'relative !important' as never,
+                        marginBottom: '20px !important',
                     })}
-                >
-                    <ListingDetails />
-                </div>
-            )}
+                />
+                {!adminPanelOpen && (
+                    <>
+                        <div
+                            className={css({
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                marginBottom: '20px',
+                            })}
+                        >
+                            <Web3ConnectButton
+                                connectModalOpen={modalOpen}
+                                setConnectModalOpen={setModalOpen}
+                                className={css({
+                                    marginLeft: 'auto',
+                                    [MOBILE]: { marginRight: '5px' },
+                                })}
+                            />
+                            <BalanceWidget
+                                className={css({
+                                    margin: '0px 10px',
+                                    [MOBILE]: { display: 'none' },
+                                })}
+                            />
+                            <DiscordLoginButton
+                                className={css({
+                                    marginRight: 'auto',
+                                    [MOBILE]: { marginLeft: '5px' },
+                                })}
+                            />
+                        </div>
+                        <div
+                            className={css({
+                                display: 'none',
+                                [MOBILE]: { display: 'block' },
+                            })}
+                        >
+                            <BalanceWidget
+                                className={css({
+                                    margin: '10px',
+                                })}
+                            />
+                        </div>
+                        <ListingGrid
+                            className={css({
+                                flex: 1,
+                                overflow: 'auto',
+                                [MOBILE]: { overflow: 'unset' },
+                            })}
+                            onSelect={onItemSelect}
+                            request={listingRequest}
+                        />
+                    </>
+                )}
+                {adminPanelOpen && (
+                    <div
+                        className={css({
+                            overflow: 'auto',
+                            flex: '1',
+                            margin: '40px',
+                            [MOBILE]: { margin: '10px' },
+                        })}
+                    >
+                        <ListingDetails />
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
