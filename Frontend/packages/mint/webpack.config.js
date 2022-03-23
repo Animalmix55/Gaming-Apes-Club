37da40943163e42c8c407dbc935e56c5e60c689f
Cory Cherven <corycherven@hotmail.com>
2022-03-23 00:56:22 -0400

    Changes mobile scaling

--- Frontend/packages/mint/webpack.config.js
+++ Frontend/packages/mint/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => ({
     entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle-03162022.js',
+        filename: 'bundle-03232022.js',
     },
     devtool: 'inline-source-map',
     mode: 'development',
