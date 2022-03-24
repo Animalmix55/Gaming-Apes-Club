dfe2bcd03d21ee373debb754d17d1933f1abc6b9
Cory Cherven <corycherven@hotmail.com>
2022-03-24 02:08:05 -0400

    Updates whitelist

--- Backend/packages/Whitelist/src/helpers/Whitelist.ts
+++ Backend/packages/Whitelist/src/helpers/Whitelist.ts
@@ -2,7 +2,9 @@ import Web3 from 'web3';
 import Whitelist from '../data/GAC_Whitelist.json';
 
 export const getWhitelist = (): string[] => {
-    return Whitelist.map((v) => v.trim()).filter(Web3.utils.isAddress);
+    return Array.from(
+        new Set(Whitelist.map((v) => v.trim()).filter(Web3.utils.isAddress))
+    );
 };
 
 export default getWhitelist;
